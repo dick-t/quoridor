45e3c13e097ac8060b9cba7f4bbcd2556383dfbc
iris-is-awesome <x.ayachan@gmail.com>
2012-10-13 16:46:38 +1100

    your mum

--- Rules.java
+++ Rules.java
@@ -2,14 +2,43 @@ package project;
 
 public class Rules extends Game {
 
-	public static boolean isLegalMove (int Xpos, int Ypos, Directions d) {
+	public Boolean isLegalMove (int Xpos, int Ypos, Directions d) {
+		if (!(isOnBoard(Xpos, Ypos, d))) {
+			return false;
+		}
 		return true;
-		//stub
+		// not finished
 	}
 	
-	public static boolean isLegalWall (int Xpos, int Ypos, WallDirections d) {
+	public Boolean isLegalWall (int Xpos, int Ypos, WallDirections d) {
+		if (!(p.nWallsLeft()>0)) {
+			return false;
+		}
+		/*if (player is blocked) {
+			return false;
+		} */
+		/*if (walls cross) {
+			return false;
+		} */
 		return true;
-		//stub
+	}
+	
+	private Boolean isOnBoard (int Xpos, int Ypos, Directions d) {
+		int pos=-1;
+		if (d == Directions.UP) {
+			pos = Ypos+1;
+		} else if (d==Directions.DOWN) {
+			pos = Ypos-1;
+		} else if (d==Directions.RIGHT) {
+			pos = Xpos+1;
+		} else if (d==Directions.LEFT) {
+			pos = Xpos-1;
+		}
+		if ((pos>=0)&&(pos<9)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 }
