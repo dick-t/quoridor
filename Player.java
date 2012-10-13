5e34729e27c9c45c4e28debab947c3bb8d477b98
iris-is-awesome <x.ayachan@gmail.com>
2012-10-13 22:45:56 +1100

    opponent field

--- Player.java
+++ Player.java
@@ -6,13 +6,17 @@ public class Player {
 	private int y;
 	private int goal;
 	private int wallsLeft;
+	public Player opponent;
 	protected Player (int n) {
-		goal = Game.N_ROWS -n;
+		goal = Game.N_ROWS -n-1;
 		x = 4;
 		y = n;
 		wallsLeft = 10;
 	}
 	
+	public void setOpponent(Player p) {
+		this.opponent = p;
+	}
 	public int getXpos() {
 		return this.x;
 	}
