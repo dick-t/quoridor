59122560212a4a7e8ac2e46f8eada03bd3c3bb09
iris-is-awesome <x.ayachan@gmail.com>
2012-10-13 19:48:10 +1100

    sdfklj

--- Rules.java
+++ Rules.java
@@ -2,43 +2,68 @@ package project;
 
 public class Rules extends Game {
 
-	public boolean isLegalMove (int Xpos, int Ypos, Directions d) {
-		if (!(isOnBoard(Xpos, Ypos, d))) {
+	public Boolean isLegalMove (int Xpos, int Ypos, Directions d, Board b) {
+		int newX = Xpos;
+		int newY = Ypos;
+		if (d==Directions.UP) {
+			newY--;
+		} else if (d==Directions.DOWN) {
+			newY++;
+		} else if (d==Directions.LEFT) {
+			newX--;
+		} else if (d==Directions.RIGHT) {
+			newX++;
+		}
+		if (!isOnBoard(newX, newY)) {
 			return false;
 		}
+		if (b.isWall(Xpos, Ypos, newX, newY)) {
+			return false;
+		}
+		// if other player in the way
 		return true;
-		// not finished
 	}
 	
-	public boolean isLegalWall (int Xpos, int Ypos, WallDirections d) {
-		if (!(p.nWallsLeft()>0)) {
+	public Boolean isLegalWall (int Xpos, int Ypos, WallDirections d, Board b) {
+		// check wall is on board
+		if ((Xpos>8)||(Ypos>8)) {
 			return false;
+		}
+
+		if (d==WallDirections.h) {
+			//existing wall
+			if (b.isWall(Xpos, Ypos, Xpos, Ypos+1)||b.isWall(Xpos+1, Ypos, Xpos+1, Ypos+1)) {
+				return false;
+			}
+			//crossing wall
+			if ((b.isWall(Xpos, Ypos, Xpos+1, Ypos))&&(b.isWall(Xpos, Ypos+1, Xpos+1, Ypos+1))) {
+				return false;
+			}
+		}
+		if (d==WallDirections.v) {
+			//existing wall
+			if (b.isWall(Xpos, Ypos, Xpos+1, Ypos)||b.isWall(Xpos, Ypos+1, Xpos+1, Ypos+1)) {
+				return false;
+			}
+			//crossing wall
+			if ((b.isWall(Xpos, Ypos, Xpos, Ypos+1))&&(b.isWall(Xpos+1, Ypos, Xpos+1, Ypos+1))) {
+				return false;
+			}
 		}
 		/*if (player is blocked) {
 			return false;
 		} */
-		/*if (walls cross) {
-			return false;
-		} */
 		return true;
 	}
 	
-	private boolean isOnBoard (int Xpos, int Ypos, Directions d) {
-		int pos=-1;
-		if (d == Directions.UP) {
-			pos = Ypos+1;
-		} else if (d==Directions.DOWN) {
-			pos = Ypos-1;
-		} else if (d==Directions.RIGHT) {
-			pos = Xpos+1;
-		} else if (d==Directions.LEFT) {
-			pos = Xpos-1;
-		}
-		if ((pos>=0)&&(pos<9)) {
-			return true;
-		} else {
+	private Boolean isOnBoard (int Xpos, int Ypos) {
+		if ((Xpos<0)||(Xpos>8)) {
 			return false;
 		}
+		if ((Ypos<0)||(Ypos>8)) {
+			return false;
+		}
+		return true;
 	}
 	
 }
